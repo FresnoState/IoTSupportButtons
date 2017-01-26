bc93c1f485fc632f4d356d7735dcd145a89b3373
Victoria Fall <victoria@imac.atapps.fresnostate.edu>
2017-01-25 16:07:34 -0800

    Moved main app component to app.js file and reset up index files
    Index files for both iOS and Android pull from the app.js file.
    This will allow Flossy to run the app on Windows with an Android
    emulator (with perhaps partial functionality depending on iOS and
    Android exclusives with React Native).

--- MobileApp/IoTApp/index.android.js
+++ MobileApp/IoTApp/index.android.js
@@ -1,10 +1,18 @@
+
+import React from 'react';
+import { AppRegistry, } from 'react-native';
+import iotapp from './app';
+        
+AppRegistry.registerComponent('IoTApp', () => iotapp);
+
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
  * @flow
  */
 
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
@@ -50,4 +58,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('IoTApp', () => IoTApp);
+AppRegistry.registerComponent('IoTApp', () => IoTApp);*/
