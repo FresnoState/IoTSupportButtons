6ad0cd82b2c4f94464f6503c754bf81e1b652222
Victoria Fall <victoria@at1106-lab-imac.009-11.csufresno.edu>
2017-06-07 11:42:21 -0700

    File clean up

--- MobileApp/IoTApp/IntervalTimeConfigurations.js
+++ MobileApp/IoTApp/IntervalTimeConfigurations.js
@@ -1,6 +1,2 @@
 //How often in milliseconds the requests will automatically be refreshed
 export var refreshMilliseconds = 60000; 
-
-//Time intervals in minutes for the request urgency level to increase
-var urgenyMinutesDifferent = [10, 20, 30];
-export {urgenyMinutesDifferent};
